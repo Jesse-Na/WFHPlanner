4730954df47e43275a6e7b88a2608eabf82da3ed
Jesse Na <jessena.2001@gmail.com>
2020-08-21 21:00:25 -0400

    Accept post request to root path

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('hello.html', name='me', age='12')
+@app.route('/', methods=['GET', 'POST'])
+def submit_task():
+    if request.method == 'POST':
+        return render_template('hello.html', name='me', age='12')
+    else:
+        return render_template('hello.html', name='me', age='12')
