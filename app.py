0baf8f89e07714aeb9316a0f40f26b803f65f58f
Jesse Na <jessena.2001@gmail.com>
2020-08-22 09:46:36 -0400

    Link stylesheet to layout.html and add more inputs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def submit_task():
     if request.method == 'POST':
+        print(request.form['email'])
         return render_template("index.html")
     else:
         return render_template("index.html")
